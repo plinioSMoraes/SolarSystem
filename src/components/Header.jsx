14e6e60633993f55c08fd669080ef5baca8cf436
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2022-07-20 13:08:15 -0500

    Requisito 1 - Feito Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// const pText = 'Sistema Solar';
+// const hText = <h1>{pText}</h1>;
+// const headerText = <header>{hText}</header>;
+
+const solarHeader = <header><h1>Sistema Solar</h1></header>;
+class Header extends React.Component {
+  render() {
+    return (solarHeader);
+  }
+}
+
+export default Header;
