afd66ab46c573f96c86cfeff51122f57b8658466
PlinioMoraes <psmoraes@inf.ufpel.edu.br>
2022-07-20 13:21:56 -0500

    Requisito 2 - Feito

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
-    return (<Header> </Header>);
+    return (
+      <main>
+        <Header> </Header>
+        <SolarSystem> </SolarSystem>
+      </main>
+    );
   }
 }
 // Iniciando o projeto
